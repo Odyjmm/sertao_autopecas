9313ee88ebed8145f38352b1b8b9d34d7ab2f051
Odyjmm <50140960+Odyjmm@users.noreply.github.com>
2026-03-31 23:51:05 -0300

    Algoritimo de busca implementado

--- app/routes/loja.py
+++ app/routes/loja.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user
+from sqlalchemy import or_
 from app.models import Produto
 
 loja = Blueprint('loja', __name__)
@@ -14,3 +15,20 @@ def produto_detalhe(id):
     produto = Produto.query.get_or_404(id)
 
     return render_template('loja/produto_detalhe.html', produto=produto, current_user=current_user)
+
+@loja.route('/busca', methods=['GET'])
+def busca():
+    termo = request.args.get('q', '')
+
+    if len(termo) < 2:
+        produtos = []
+    else:
+        produtos = Produto.query.filter(
+            or_(
+                Produto.nome.ilike(f'%{termo}%'),
+                Produto.codigo.ilike(f'%{termo}%'),
+                Produto.categoria.ilike(f'%{termo}%')
+            )
+        ).all()
+
+    return render_template('loja/busca.html', produtos=produtos, termo=termo)

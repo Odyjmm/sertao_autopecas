ce4ce13f7dd60c7e7be9355ed362ecf3baa38e98
Odyjmm <50140960+Odyjmm@users.noreply.github.com>
2026-04-08 19:56:54 -0300

    Imagem padrão adicionada na criação

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Produto(db.Model):
     categoria = db.Column(db.String(100), nullable=False)
     preco = db.Column(db.Float, nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
-    imagem = db.Column(db.String(200), nullable=True)
+    imagem = db.Column(db.String(200), nullable=True, default='sem_imagem.jpg')
 
 class Pedido(db.Model):
     id = db.Column(db.Integer, primary_key=True)
